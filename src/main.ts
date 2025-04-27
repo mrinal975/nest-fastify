490af4190d336cdb3cd01dee2a9373d9647ddfb5
Mrinal <mrinal@nagorik.tech>
2025-04-27 18:48:25 +0600

    init commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.useGlobalPipes(new ValidationPipe()); // Add validation globally
+  await app.listen(process.env.PORT || 3000, '0.0.0.0'); // Port from env or 3000
+}
+bootstrap();
